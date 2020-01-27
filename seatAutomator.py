db393dc4526e11bba60ea94a9055809756f79863
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2020-01-27 00:27:50 -0500

    implemented priority queue

--- seatAutomator.py
+++ seatAutomator.py
@@ -18,13 +18,16 @@ class Theatre:
             self.leftRows.append((copy.deepcopy(leftRow)))
             self.rightRows.append((copy.deepcopy(rightRow)))
 
-
-    def seat(self, groups):
-
 class Group: 
     
     def __init__(self, groupInfo):
         (self.name, self.size, self.priority) = groupInfo
+
+    def __lt__(self, other):
+
+        if self.priority == other.priority:
+            return self.size > other.size  
+        return self.priority < other.priority
 
     def getName(self):
         return self.name
@@ -35,24 +38,33 @@ class Group:
     def getPriority(self):
         return self.priority
 
-def makeGroupQueue():
+def makeGroupQueue(groups):
     q = queue.PriorityQueue()
-
-
-
-
-
+    for g in groups:
+        q.put(g)
+    return q
+    
 
 t = Theatre(3, 4, 5)
 sampleGroups = [
     ("A", 4, 1),
-    ("B", 2, 3)
-    ("C", 2, 1)
-    ("D", 3, 2)
-    ("E", 4, 3)
-    ("F", 1, 2)
-    ("G", 1, 2)
+    ("B", 2, 3),
+    ("C", 2, 1),
+    ("D", 3, 2),
+    ("E", 4, 3),
+    ("F", 1, 2),
+    ("G", 1, 2),
     ("H", 3, 1) ]
-print(t.leftRows)
-print(t.rightRows)
+
+# Expected Ordering: A, H, C, D, F/G, E, B 
+
+groupObjList = []
+for g in sampleGroups:
+    groupObj = Group(g)
+    groupObjList.append(groupObj)
+q = makeGroupQueue(groupObjList)
+while not q.empty():
+    o = q.get(0)
+    print ("Name: " + o.getName())
+
 
